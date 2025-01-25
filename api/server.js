19ef1542f3eb6e3e16bbf564aa4139bc4b2a6d9d
Jami Raza <jamiraza361@gmail.com>
2025-01-25 04:32:59 -0800

    vercel file added

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const authRoutes = require("./routes/authRoutes");
+const authRoutes = require("../routes/authRoutes");
 
 dotenv.config();
 
